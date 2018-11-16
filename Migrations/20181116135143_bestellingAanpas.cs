70d8c9700b7514355d988d07f8a31b40e9d3d70c
Hakoen <0949698@hr.nl>
2018-11-16 14:55:23 +0100

    Bestelling dingen joe

---
+++ Migrations/20181116135143_bestellingAanpas.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace kamerplantModel.Migrations
+{
+    public partial class bestellingAanpas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "adres",
+                table: "klant");
+
+            migrationBuilder.DropColumn(
+                name: "adres",
+                table: "geregistreerdeklant");
+
+            migrationBuilder.AddColumn<string>(
+                name: "adres",
+                table: "bestelling",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "adres",
+                table: "bestelling");
+
+            migrationBuilder.AddColumn<string>(
+                name: "adres",
+                table: "klant",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "adres",
+                table: "geregistreerdeklant",
+                nullable: true);
+        }
+    }
+}
