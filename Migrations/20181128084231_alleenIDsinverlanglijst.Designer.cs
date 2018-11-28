a72f48a89f53c57a64455122537622dcb37ad5d1
Hakoen <0949698@hr.nl>
2018-11-28 09:43:58 +0100

    Verlanglijst alleen met ID's

--- Migrations/20181128084231_alleenIDsinverlanglijst.Designer.cs
+++ Migrations/20181128084231_alleenIDsinverlanglijst.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace kamerplantModel.Migrations
 {
     [DbContext(typeof(kamerplantContext))]
-    partial class kamerplantContextModelSnapshot : ModelSnapshot
+    [Migration("20181128084231_alleenIDsinverlanglijst")]
+    partial class alleenIDsinverlanglijst
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
