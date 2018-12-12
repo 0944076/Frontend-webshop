4dca8fb37160e38433ac1239fbe8371893ed2a2c
Hakoen <0949698@hr.nl>
2018-12-12 15:27:20 +0100

    De winkelmand haalt producten op!!!!!

--- src/pages/WinkelMand/index.jsx
+++ src/pages/WinkelMand/index.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import request from 'superagent';
+import axios from 'axios';
 import WinkelmandItem from "../../components/WinkelmandItem";
 import { Link } from "react-router-dom";
+import Loader from '../../components/Loading';
 
 // layout
 import LayoutAccount from "../../layout/Account";
@@ -15,76 +16,114 @@ class WinkelMand extends Component {
     super(props);
     this.retrieveProduct = this.retrieveProduct.bind(this);
     this.productToState = this.productToState.bind(this);
+    this.outputState = this.outputState.bind(this);
 
     this.state = {
       loading: true,
       producten: []
     };
 
-  }
-
-  componentWillReceiveProps(nextProps) {
-    const currentParams = this.props.match.params;
-    const nextParams = nextProps.match.params;
-    if (currentParams.page !== nextParams.page) {
-      this.getProducts(nextParams.page,this.state.query);
-    }
-
-  }
-
-  componentDidMount() {
-    console.log("this.props", this.props);
-    this.getProducts(this.props.match.params.page,this.state.query);
+    this.productToState();
 
   }
 
 
   productToState(){
       const items = JSON.parse(window.localStorage.getItem('cart'));
+      console.log('Lokaal: ' + JSON.stringify(items));
       let result = [];
 
       for(let i = 0; i < items.length; i++){
-        console.log(i + 'e element: ' + this.retrieveProduct(items[i].id));
-        result.push(this.retrieveProduct(items[i].id));
+        const product = this.retrieveProduct(items[i].id)
+        .then((res) => {
+          result.push({product: res, aantal: items[i].qty});
+          //console.log('result array:' + result.map((element) => JSON.stringify(element)));
+          if(result.length === items.length){  
+            this.setState({producten: result});
+            this.setState({loading: false});
+          }
+          }
+        );
       }
-
-      console.log('prod to stat: ' + JSON.stringify(result));
-      return result;
     }
 
-    retrieveProduct(id){
-      
-      console.log('retrieve ' + id + ': ' + JSON.stringify(result.data));
-      return result.data;
+    async retrieveProduct(id){
+      return await axios.get('http://localhost:5000/api/product/' + id)
+      .then((res) =>{
+        return res.data;
+      });
     }
 
-    getProducts(page,query) {
-      request
-        .get(`http://localhost:5000/api/product?pageSize=9999`)
-        .then(response => {
-          this.setState({
-            response: response.body,
-            loading: false
-          });
-        });
+    outputState(){
+      const stateObject = this.state.producten;
+      console.log('Complete state: ' + JSON.stringify(stateObject));
+      console.log('Lengte state: ' + stateObject.length);
+      console.log('producten onderdeel: ' + JSON.stringify(stateObject[0]));
     }
 
   render() {
-    console.log('producten: ' + this.state.producten);
-    return (   
-      <React.Fragment>
-        <LayoutAccount className="SignUp" simple>
-          <div className="wrapper">
-            <SimpleHeading
-              title="Winkelmand"
-              description="Een overzicht van de door u geselecteerde producten:"
-            />
-            <WinkelmandItem foto='https://www.tuinflora.com/media/catalog/product/cache/5/image/500x/9df78eab33525d08d6e5fb8d27136e95/F/D/FD16894WH_3.jpg' titel='Kanker geile plant' prijs='420' />
-            <button>Plaats bestelling</button>
-          </div>
-        </LayoutAccount>
-      </React.Fragment>
-    );
+    if (!this.state.loading){
+      this.outputState();
+      return (   
+        <React.Fragment>
+          <LayoutAccount className="SignUp" simple>
+            <div className="wrapper">
+              <SimpleHeading
+                title="Winkelmand"
+                description="Een overzicht van de door u geselecteerde producten:"
+              />
+              <div className="betaalOverzicht">
+                  <h1>Betaal overzicht</h1><br />
+                  Totaal: <br />
+                  
+                  <table>
+                    <tbody>
+                      <tr>
+                        <td>Verzending:</td>
+                        <td>€4,95</td>
+                      </tr>
+                      <tr>
+                        <td>Totaal excl. BTW:</td>
+                        <td>€104,95</td>
+                      </tr>
+                      <tr>
+                        <td>6% BTW: </td>
+                        <td>€6</td>
+                      </tr>
+                      <tr>
+                        <td><b>Totaal incl. BTW:</b></td>
+                        <td><b>€110,95</b></td>                    
+                      </tr>
+                    </tbody>
+                  </table>   
+                  <button>Plaats bestelling</button>
+              </div>
+              <div className="paginaFrame">
+                <div className="mandFrame">
+                  <WinkelmandItem foto='https://www.tuinflora.com/media/catalog/product/cache/5/image/500x/9df78eab33525d08d6e5fb8d27136e95/F/D/FD16894WH_3.jpg' titel='Kanker geile plant' prijs='420' /><br />
+                  <WinkelmandItem foto='https://www.tuinflora.com/media/catalog/product/cache/5/image/500x/9df78eab33525d08d6e5fb8d27136e95/F/D/FD16894WH_3.jpg' titel='Kanker geile plant' prijs='420' /><br />
+                  <WinkelmandItem foto='https://www.tuinflora.com/media/catalog/product/cache/5/image/500x/9df78eab33525d08d6e5fb8d27136e95/F/D/FD16894WH_3.jpg' titel='Kanker geile plant' prijs='420' /><br />
+                  <WinkelmandItem foto='https://www.tuinflora.com/media/catalog/product/cache/5/image/500x/9df78eab33525d08d6e5fb8d27136e95/F/D/FD16894WH_3.jpg' titel='Kanker geile plant' prijs='420' /><br />
+                  <WinkelmandItem foto='https://www.tuinflora.com/media/catalog/product/cache/5/image/500x/9df78eab33525d08d6e5fb8d27136e95/F/D/FD16894WH_3.jpg' titel='Kanker geile plant' prijs='420' /><br />
+                  <WinkelmandItem foto='https://www.tuinflora.com/media/catalog/product/cache/5/image/500x/9df78eab33525d08d6e5fb8d27136e95/F/D/FD16894WH_3.jpg' titel='Kanker geile plant' prijs='420' /><br />
+                  <WinkelmandItem foto='https://www.tuinflora.com/media/catalog/product/cache/5/image/500x/9df78eab33525d08d6e5fb8d27136e95/F/D/FD16894WH_3.jpg' titel='Kanker geile plant' prijs='420' /><br />
+                  <WinkelmandItem foto='https://www.tuinflora.com/media/catalog/product/cache/5/image/500x/9df78eab33525d08d6e5fb8d27136e95/F/D/FD16894WH_3.jpg' titel='Kanker geile plant' prijs='420' /><br />
+                  <WinkelmandItem foto='https://www.tuinflora.com/media/catalog/product/cache/5/image/500x/9df78eab33525d08d6e5fb8d27136e95/F/D/FD16894WH_3.jpg' titel='Kanker geile plant' prijs='420' /><br />
+                  <WinkelmandItem foto='https://www.tuinflora.com/media/catalog/product/cache/5/image/500x/9df78eab33525d08d6e5fb8d27136e95/F/D/FD16894WH_3.jpg' titel='Kanker geile plant' prijs='420' /><br />
+                  <WinkelmandItem foto='https://www.tuinflora.com/media/catalog/product/cache/5/image/500x/9df78eab33525d08d6e5fb8d27136e95/F/D/FD16894WH_3.jpg' titel='Kanker geile plant' prijs='420' /><br />
+                  <WinkelmandItem foto='https://www.tuinflora.com/media/catalog/product/cache/5/image/500x/9df78eab33525d08d6e5fb8d27136e95/F/D/FD16894WH_3.jpg' titel='Kanker geile plant' prijs='420' /><br />
+                  <WinkelmandItem foto='https://www.tuinflora.com/media/catalog/product/cache/5/image/500x/9df78eab33525d08d6e5fb8d27136e95/F/D/FD16894WH_3.jpg' titel='Kanker geile plant' prijs='420' /><br />
+                  <WinkelmandItem foto='https://www.tuinflora.com/media/catalog/product/cache/5/image/500x/9df78eab33525d08d6e5fb8d27136e95/F/D/FD16894WH_3.jpg' titel='Kanker geile plant' prijs='420' /><br />
+                  <WinkelmandItem foto='https://www.tuinflora.com/media/catalog/product/cache/5/image/500x/9df78eab33525d08d6e5fb8d27136e95/F/D/FD16894WH_3.jpg' titel='Kanker geile plant' prijs='420' /><br />
+                </div>
+              </div>
+            </div>
+          </LayoutAccount>
+        </React.Fragment>
+      );
+    } else {
+      return <Loader />
+    }
   }
 }
 export default WinkelMand;
