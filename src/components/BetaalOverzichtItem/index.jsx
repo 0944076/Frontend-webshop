583fc0034206ad4386094cd78f71414467933061
Hakoen <0949698@hr.nl>
2018-12-20 15:33:48 +0100

    Betaaloverzicht doet geen gekke commas

--- src/components/BetaalOverzichtItem/index.jsx
+++ src/components/BetaalOverzichtItem/index.jsx
@@ -9,7 +9,7 @@ class BetaalOverzichtItem extends Component {
     return <tr>
         <td>{this.props.aantal}</td>
         <td>{(this.props.naam.length < 24)? this.props.naam : this.props.naam.substr(0,20) + '...'}</td>
-        <td>€{this.props.aantal * this.props.prijs}</td>
+        <td>€{(this.props.aantal * this.props.prijs).toFixed(2)}</td>
     </tr>
   }
 }
